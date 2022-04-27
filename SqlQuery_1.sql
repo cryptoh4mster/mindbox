addbb0144fecae1cacf2a72dba0342d8de46dbd3
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-04-27 16:29:49 +0300

    Задание 2 - SQL запрос

---
+++ SqlQuery_1.sql
@@ -0,0 +1,14 @@
+﻿--В базе данных MS SQL Server есть продукты и категории. Одному продукту может соответствовать много категорий, в одной категории может быть много продуктов.
+--Напишите SQL запрос для выбора всех пар «Имя продукта – Имя категории». Если у продукта нет категорий, то его имя все равно должно выводиться.
+-----------
+--Products-   --Products_Categories-   --Categories-
+--Id-------   --ProductId-----------   --Id---------
+--Name-----   --CategoryId----------   --Name-------
+-----------   ----------------------   -------------
+
+SELECT Products.Name, Categories.Name
+FROM Products
+LEFT JOIN Products_Categories
+	ON Products.Id=Products_Categories.ProductId
+LEFT JOIN Categories 
+	ON Categories.Id=Products_Categories.CategoryId
