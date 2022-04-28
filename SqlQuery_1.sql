2119f5eee1493fd1864575800f862b35096120ec
cryptoh4mster <l1ve_to_w1n@mail.ru>
2022-04-28 12:09:17 +0300

    Добавлен второй вариант sql запроса

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -12,3 +12,13 @@ LEFT JOIN Products_Categories
 	ON Products.Id=Products_Categories.ProductId
 LEFT JOIN Categories 
 	ON Categories.Id=Products_Categories.CategoryId
+
+
+SELECT Products.Name, Categories.Name
+FROM Products_Categories
+JOIN Categories
+	ON Categories.Id=Products_Categories.CategoryId
+RIGHT JOIN Products
+	ON Products.Id=Products_Categories.ProductId
+
+
